75bbc9ae081a373ff298f42564ccfe3a3f8b26e5
Evilen Barreto <159492801+evbarretoo@users.noreply.github.com>
2024-10-25 09:05:56 -0300

    Update index.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,7 +3,7 @@ export default function Card() {
         <div>
             <div className="container">
                 <div className="image"></div>
-                <img className="image" src="../../../public/the-amazing-spider-man.jpg"></img>
+                <img className="image" src="./public/the-amazing-spider-man.jpg"></img>
                 <div className="about">
                     <h1>Pôster: O Espetacular Homem-Aranha</h1>
                     <p>Parararipturura</p>
